7034a4ad8950dbd67914f6268ca60ed12cb238a6
lzhnb <eezhihaoliang@mail.scut.edu.cn>
2022-10-19 15:43:09 +0800

    format the ray casting code, next is to realize the interface

--- src/prim3d/Utility/ray_cast_optix.h
+++ src/prim3d/Utility/ray_cast_optix.h
@@ -2,8 +2,8 @@
 #include <iostream>
 #include <memory>
 
-#include "optix_ext/utils.h"
 #include "optix_ext/launch_parameters.h"
+#include "optix_ext/utils.h"
 
 namespace prim3d {
 template <typename T>
@@ -12,40 +12,18 @@ struct SbtRecord {
     T data;
 };
 
+// abstract class of RayCaster
 class RayCaster {
 public:
-    RayCaster() {
-        /* init optix*/
-        // Initialize CUDA with a no-op call to the the CUDA runtime API
-        cudaFree(nullptr);
+    RayCaster() {}
+    ~RayCaster() {}
 
-        // Initialize the OptiX API, loading all API entry points
-        OPTIX_CHECK_THROW(optixInit());
-
-        // Specify options for this context. We will use the default options.
-        m_options = {};
-
-        // Associate a CUDA context (and therefore a specific GPU) with this
-        // device context
-        m_cuCtx = 0;  // NULL means take the current active context
-
-        m_context = nullptr;
-        OPTIX_CHECK_THROW(optixDeviceContextCreate(m_cuCtx, &m_options, &m_context));
-    };
-    ~RayCaster(){};
-    void build_gas();
-    void build_pipeline();
-
-    void invoke(const RayCast::Params& params, const int32_t height, const int32_t width);
-
-private:
-    OptixDeviceContextOptions m_options;
-    OptixDeviceContext m_context;
-    CUcontext m_cuCtx;
-    OptixTraversableHandle m_gas_handle;
-    CUdeviceptr m_gas_output_buffer;
-    OptixModule m_module;  // The output module
-    OptixPipeline m_pipeline;
-    OptixShaderBindingTable m_sbt;
+    virtual void build_gas();
+    virtual void build_pipeline();
+    virtual void invoke(
+        const RayCast::Params& params, const int32_t height, const int32_t width) = 0;
 };
+
+// function to create an implementation of RayCaster
+RayCaster* create_raycaster();
 }  // namespace prim3d
--- src/pybind/bindings.cpp
+++ src/pybind/bindings.cpp
@@ -4,10 +4,16 @@
 #include <torch/extension.h>
 
 #include <Core/utils.h>
+#include <Utility/ray_cast_optix.h>
 
 namespace py = pybind11;
 
 // TODO: decouple the pybind11 module
 namespace prim3d {
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) { m.def("test", &test); }
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("test", &test);
+    
+    py::class_<RayCaster>(m, "RayCaster");
+    m.def("create_raycaster", &create_raycaster);
+}
 }  // namespace prim3d
